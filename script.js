a71e3bb4b625f106f03e2213f25398bee900ab83
Phelipe <lipe.macel@gmail.com>
2023-01-25 14:26:52 -0300

    url atualizada

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ window.onload = function (){
         b = document.getElementById("TextB").value;
         
         //Executo o fetch que faz a requisição para a API e atualizo o HTML com o resultado
-        //Trocar link para 'http://localhost:5000/api...' caso queira rodar localmente
-        fetch(`https://calculadorapitagoras.glitch.me/api?a=${a}&b=${b}`)
+        fetch(`http://localhost:5000/api?a=${a}&b=${b}`)
             .then(response => response.text())
             .then(data => {
                 document.getElementById("LadoC").innerHTML = `Lado C: ${data}`;
